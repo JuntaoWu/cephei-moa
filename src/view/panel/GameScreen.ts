bdf95e78fd56b08f98cbe51891849302a197dd03
Tony Juntao. Wu <wujuntaocn@outlook.com>
2018-08-13 12:17:42 +0800

    merge commit

--- src/view/panel/GameScreen.ts
+++ src/view/panel/GameScreen.ts
@@ -55,7 +55,6 @@ module game {
         public isLastPlayer: boolean = false;
         public isVoteEnd: boolean = false;
         public voteing: string;
-        public isSpeaking: boolean = false;
         public isWaitNextTurn: boolean = false;
         public isWaitTouRen: boolean = false;
         public isChoosingNextText: boolean = false;
