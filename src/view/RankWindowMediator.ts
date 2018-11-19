e47ef324c875adc1c9ac01e436b1eb1081a5e807
chenlunsen <563575103@qq.com>
2018-11-19 18:20:16 +0800

    rank change

--- src/view/RankWindowMediator.ts
+++ src/view/RankWindowMediator.ts
@@ -8,76 +8,59 @@ module game {
             super(RankWindowMediator.NAME, viewComponent);
             super.initializeNotifier("ApplicationFacade");
 
-            this.rankWindow.winRateButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.winRateButtonClick, this);
-            this.rankWindow.roleButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.roleButtonClick, this);
-            this.rankWindow.gameNumButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.gameNumButtonClick, this);
             this.rankWindow.backButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.backButtonClick, this);
-            
-            for (let key in this.buttonList) {
-                this.buttonList[key].forEach(i => {
-                    i.btn.addEventListener(egret.TouchEvent.TOUCH_TAP, (event: egret.TouchEvent) => {
-                        this.buttonList[key].forEach(item => {
-                            if (item.btn.selected) item.btn.selected = false;
-                        })
-                        event.currentTarget.selected = true;
-                        this.rankListSort(i.key)
-                    }, this);
-                });
-            }
             this.initData();
         }
 
         public async initData() {
-            this.rankList = [];
-            // for (let i = 1; i < 21; i++) {
-            //     this.rankList.push({
-            //         key: i,
-            //         name: "name"+i,
-            //         score: i,
-            //         totalGame: i,
-            //     })
-            // }
-            this.rankList = RES.getRes("rank_json") as Array<any>;
-            this.rankList.sort((a, b) => {
-                return a.score > b.score ? -1 : 1;
+            let filterList = [
+                [ 
+                    { key: "0", name: "胜率榜" }
+                    , { key: "1", name: "场次榜" } 
+                ], 
+                [
+                    { key: "0", name: "全部人数" }
+                    , { key: "6", name: "六人局" }
+                    , { key: "7", name: "七人局" }
+                    , { key: "8", name: "八人局" }
+                ],
+                [
+                    { key: "0", name: "全部角色" }
+                    , { key: "1", name: "许愿" }
+                    , { key: "2", name: "方震" }
+                    , { key: "3", name: "黄烟烟" }
+                    , { key: "4", name: "木户加奈" }
+                    , { key: "5", name: "姬云浮" }
+                    , { key: "6", name: "老朝奉" }
+                    , { key: "7", name: "药不然" }
+                    , { key: "8", name: "郑国渠" }
+                ],
+                [
+                    { key: "0", name: "10场以上" }
+                    , { key: "1", name: "50场以上" }
+                    , { key: "2", name: "100场以上" }
+                ]
+            ]
+            this.filterKeyList = [];
+            this.nameList = ["rateTime", "gameType", "role", "other"];
+            this.nameList.forEach((v, i) => {
+                this.rankWindow[`${v}Filter`].addEventListener(egret.TouchEvent.TOUCH_TAP, () => this.showFilterGroup(i), this);
+                
+                this.rankWindow[`${v}List`].visible = false;
+                this.rankWindow[`${v}List`].addEventListener(eui.ItemTapEvent.ITEM_TAP, () => this.selectedItem(i), this);
+                this.rankWindow[`${v}List`].dataProvider = new eui.ArrayCollection(filterList[i]);
+                this.rankWindow[`${v}List`].itemRenderer = CityItemRenderer;
+
+                this.rankWindow[v] = filterList[i][0].name;
+                this.filterKeyList[i] = filterList[i][0].key;
             })
-            this.rankList.forEach((item, index) => {
-                item.key = index + 1;
-                item.score = `${item.score}局`
-            })
-            this.rankWindow.rankList.dataProvider = new eui.ArrayCollection(this.rankList);
-            this.rankWindow.rankList.itemRenderer = RankListItemRenderer;
-            this.rankWindow.winRateButton.visible = false;
-            this.rankWindow.roleButton.visible = false;
-            this.rankWindow.gameNumButton.visible = false;
+            this.setRankList();
         }
 
         private rankList: Array<any>;
-        private buttonList: any = {
-            winRate: [
-                { btn: this.rankWindow.totalRate, key: "totalRate"},
-                { btn: this.rankWindow.gameRate6, key: "gameRate6"},
-                { btn: this.rankWindow.gameRate7, key: "gameRate7"},
-                { btn: this.rankWindow.gameRate8, key: "gameRate8"},
-            ],
-            role: [
-                { btn: this.rankWindow.roleTeamXu, key: "roleTeamXu"},
-                { btn: this.rankWindow.roleTeamLao, key: "roleTeamLao"},
-                { btn: this.rankWindow.roleAuthRate, key: "roleAuthRate"},
-                { btn: this.rankWindow.roleAuthPlayer, key: "roleAuthPlayer"},
-                { btn: this.rankWindow.roleXu, key: "roleXu"},
-                { btn: this.rankWindow.roleFang, key: "roleFang"},
-                { btn: this.rankWindow.roleHuang, key: "roleHuang"},
-                { btn: this.rankWindow.roleMu, key: "roleMu"},
-                { btn: this.rankWindow.roleJi, key: "roleJi"},
-                { btn: this.rankWindow.roleLao, key: "roleLao"},
-                { btn: this.rankWindow.roleYao, key: "roleYao"},
-                { btn: this.rankWindow.roleZheng, key: "roleZheng"},
-            ],
-            gameNum: [
-                { btn: this.rankWindow.totalPlay, key: "totalPlay"},
-            ],
-        };
+
+        private nameList: Array<string>;
+        private filterKeyList: Array<string>;
 
         private rankListSort(key: string) {
             console.log(key)
@@ -87,25 +70,49 @@ module game {
             this.rankWindow.close();
         }
 
-        private winRateButtonClick(event: egret.TouchEvent) {
-            // this.rankWindow.showGameNumSwitch = this.rankWindow.showRoleSwitch = false;
-            // this.rankWindow.showWinRateSwitch = !this.rankWindow.showWinRateSwitch;
-            this.rankWindow.winRateButton.selected = true;
-            this.rankWindow.roleButton.selected = this.rankWindow.gameNumButton.selected = false;
+        private showFilterGroup(index: number) {
+            this.nameList.forEach((v, i) => {
+                this.rankWindow[`${v}List`].visible = i == index ? !this.rankWindow[`${v}List`].visible : false;
+            })
         }
 
-        private roleButtonClick(event: egret.TouchEvent) {
-            // this.rankWindow.showGameNumSwitch = this.rankWindow.showWinRateSwitch = false;
-            // this.rankWindow.showRoleSwitch = !this.rankWindow.showRoleSwitch;
-            this.rankWindow.roleButton.selected = true;
-            this.rankWindow.winRateButton.selected = this.rankWindow.gameNumButton.selected = false;
+        private selectedItem(index: number) {
+            let selectedItem = this.rankWindow[`${this.nameList[index]}List`].selectedItem;
+            if (this.filterKeyList[index] != selectedItem.key) {
+                this.filterKeyList[index] = selectedItem.key;
+                this.rankWindow[`${this.nameList[index]}List`].visible = false;
+                this.rankWindow[this.nameList[index]] = selectedItem.name;
+                this.setRankList();
+            }
         }
 
-        private gameNumButtonClick(event: egret.TouchEvent) {
-            // this.rankWindow.showWinRateSwitch = this.rankWindow.showRoleSwitch = false;
-            // this.rankWindow.showGameNumSwitch = !this.rankWindow.showGameNumSwitch;
-            this.rankWindow.gameNumButton.selected = true;
-            this.rankWindow.roleButton.selected = this.rankWindow.winRateButton.selected = false;
+        private setRankList() {
+            this.rankList = [];
+            this.getRank().then(res => {
+                console.log(res)
+                // this.rankList = res;
+            }) 
+        }
+
+        private getRank() {
+            return new Promise((resolve, reject) => {
+                var request = new egret.HttpRequest();
+                request.responseType = egret.HttpResponseType.TEXT;
+                request.open(`${game.Constants.Endpoints.service}rank/`, egret.HttpMethod.POST);
+                request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.send();
+                request.addEventListener(egret.Event.COMPLETE, (event: egret.Event) => {
+                    let req = <egret.HttpRequest>(event.currentTarget);
+                    let res = JSON.parse(req.response);
+                    if (res.error) {
+                        console.error(res.message);
+                        reject(res.message);
+                    }
+                    else {
+                        resolve();
+                    }
+                }, this);
+            })
         }
 
         public listNotificationInterests(): Array<any> {
