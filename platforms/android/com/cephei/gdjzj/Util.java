b2dd2c0f798b790fe9b7efeee762f8797bca15e6
Tony Juntao. Wu <wujuntaocn@outlook.com>
2018-12-07 18:49:27 +0800

    backup android.

---
+++ platforms/android/com/cephei/gdjzj/Util.java
@@ -0,0 +1,12 @@
+package com.toolbar.childar;
+
+/**
+ * Created by Juntao on 11/22/2018.
+ */
+
+public class Util {
+    public static boolean isNullOrBlank(String s)
+    {
+        return (s==null || s.trim().equals(""));
+    }
+}
